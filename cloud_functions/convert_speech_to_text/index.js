077c073789b089ae32c73fea113d056485c0057c
Elishuwon Mitchell <elishuwonmitchell@Elishuwons-MacBook-Pro.local>
2018-08-13 23:18:19 -0400

    save converted speech to firestore from cloud function

--- cloud_functions/convert_speech_to_text/index.js
+++ cloud_functions/convert_speech_to_text/index.js
@@ -1,33 +1,49 @@
 const speech = require('@google-cloud/speech');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+// Firestore initialization
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+db.settings({ timestampsInSnapshots: true }); // Firestore date setting
+
+// Speech API Client initialization
+const client = new speech.SpeechClient();
 
 exports.convertSpeech = (data, context) => {
-    const file = data;
+	const file = data;
 
- 	console.log(`Event ${context.eventId}: Starting to process speech file ${file.name} created on ${file.timeCreated}`);
-
-	const client = new speech.SpeechClient();
+	console.log(`Event ${context.eventId}: Starting to process speech file ${file.name} created on ${file.timeCreated}`);
 
 	const bucketName = 'eli-mitchell';
 
 	const audio = {
 		uri: `gs://${bucketName}/${file.name}`
-    };
+	};
 	const config = {
 		encoding: 'LINEAR16',
 		sampleRateHertz: 16000,
 		languageCode: 'en-US'
 	};
-
 	const request = {
 		audio: audio,
 		config: config
 	};
-	
-	client.recognize(request).then((results) => {
-      console.log(results);
-      const transcription = results[0].results[0].alternatives[0].transcript;
-      console.log("Translated text:", transcription);
+
+	client.recognize(request).then(async (results) => {
+		console.log(results);
+		const transcription = results[0].results[0] ? results[0].results[0].alternatives[0].transcript: "Error: Could not convert speech to text";
+		console.log("Translated text:", transcription);
+		try {
+			await db.collection('speech').add({
+				text: transcription,
+				date: new Date()
+			});	
+			console.log("Firestore save success.");
+		} catch (error) {
+			console.error("Firestore save failed: ", error);
+		}
 	}).catch((err) => {
-      console.error('ERROR:', err);
+		console.error('ERROR:', err);
 	});
 };
